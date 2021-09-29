d7c62f696486406ef5b8159cf01b8315ceb12989
p22cobrien <p22cobrien@fairfieldprep.org>
2021-09-29 10:20:19 -0400

    add hw3-2-1.py

---
+++ hw3-2-1.py
@@ -0,0 +1,25 @@
+# Author: CMOB 9/29/2021
+score = int(input("Please enter your grade: "))
+
+if score < 59:
+    print("You have a F")
+elif score < 65:
+    print("You have a D")
+elif score < 70:
+    print("You have a D+")
+elif score < 73:
+    print("You have a C-")
+elif score < 77:
+    print("You have a C")
+elif score < 80:
+    print("You have a C+")
+elif score < 83:
+    print("You have a B-")
+elif score < 87:
+    print("You have a B")
+elif score < 90:
+    print("You have a B+")
+elif score < 93:
+    print("You have an A-")
+elif score <= 100:
+    print("You have an A")
